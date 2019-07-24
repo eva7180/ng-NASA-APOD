1716b87b3a737073ad7eff92b1752cc59000de11
Eva Stolz <evasmail@gmail.com>
2019-07-24 20:00:22 +0200

    Set up service to communicate with API

---
+++ src/app/apod.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApodService {
+
+  constructor(private http: HttpClient) { }
+
+  getAPOD = () => {
+    return this.http.get('https://api.nasa.gov/planetary/apod?api_key=eNaykbtMaWbovl71dcInVBGQqEEJkoWbQsVAIb9o')
+  }
+}
