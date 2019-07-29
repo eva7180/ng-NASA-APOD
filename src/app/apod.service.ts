4656ca04c7e63d99d72a37fab595bf9d33f1f74f
Eva Stolz <evasmail@gmail.com>
2019-07-29 22:26:20 +0200

    Add config var in production

--- src/app/apod.service.ts
+++ src/app/apod.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { APIkey } from './api-keys';
 // import { Observable } from 'rxjs/Observable';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApodService {
+  if (environment.envName === 'prod') {
+    let APIkey = process.env.APIkey;
+  };
 
   constructor(private http: HttpClient) { }
 
